1acc4b724c78b43cfcca5b51332d737197167f43
milagro2 <morenoseijs06@gmail.com>
2024-03-01 13:06:54 +0100

    laravel_coming_soon

--- docs/load_notes.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Load notes from the file (you may want to fetch from a database for a real application)
-$notes = file("notes.txt");
-
-// Display notes
-foreach ($notes as $note) {
-    echo "<div class='note'>$note</div>";
-}
--- docs/save_note.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Check if the note is sent
-if (isset($_POST['note'])) {
-    $note = $_POST['note'];
-
-    // Append the note to the file (you may want to store in a database for a real application)
-    file_put_contents("notes.txt", $note . PHP_EOL, FILE_APPEND);
-}
